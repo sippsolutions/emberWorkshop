6c6b717b1ae0cd692b90db60455b6e88b959221f
Simon Sippert <s.sippert@sippsolutions.de>
2014-04-08 16:12:43 +0200

    Location Markers

--- app/scripts/models/location.js
+++ app/scripts/models/location.js
@@ -10,15 +10,33 @@ Emberworkshop.Location = Ember.Object.extend({
     updateWeather: function () {
         var _this = this;
 
+        // data for query string
+        var data = {
+            units: 'metric'
+        }
+
+        // we got coordinates
+        if (this.get('coords.lat')) {
+            data.lat = this.get('coords.lat');
+            data.lon = this.get('coords.lon');
+        }
+        // we only got the place's name
+        else {
+            data.q = this.get('name');
+        }
+
+        // send the request
         Ember.$.ajax({
             url: "http://api.openweathermap.org/data/2.5/weather",
-            data: {
-                units: 'metric',
-                q: this.get('name')
-            }
+            data: data
         }).done(function (data) {
-            _this.set('coords', Ember.Object.create(data.coord));
+            _this.set('name', data.name);
+
+            if (!_this.get('coords.lat')) {
+                _this.set('coords', Ember.Object.create(data.coord));
+            }
+
             _this.set('weather', Ember.Object.create(data.main));
         });
-    }.observes('name')
+    }.observes('coords')
 });
--- app/scripts/views/MapView.js
+++ app/scripts/views/MapView.js
@@ -3,9 +3,51 @@ Emberworkshop.MapView = Ember.View.extend({
     classNames: 'map',
 
     didInsertElement: function () {
+        var _this = this;
+        var position = new google.maps.LatLng(this.get('coords.lat'), this.get('coords.lon'));
+
+        // Map
         var map = new google.maps.Map(Ember.$(this.get('element')).get(0), {
-            center: new google.maps.LatLng(this.get('coords').get('lat'), this.get('coords').get('lon')),
+            center: position,
             zoom: 8
+        });
+
+        // Marker
+        var marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: this.get('name')
+        });
+
+        // infowindow
+        var info = null;
+
+        // open marker on click
+        google.maps.event.addListener(marker, 'click', function () {
+            // close old window
+            if (info) {
+                info.close();
+                info.setMap(null);
+            }
+
+            info = new google.maps.InfoWindow({
+                content: _this.get('name')
+            });
+
+            info.open(map, marker);
+        });
+
+        // open marker on click
+        google.maps.event.addListener(map, 'dragend', function () {
+            // close old window
+            if (info) {
+                info.close();
+                info.setMap(null);
+            }
+
+            var mapCenter = map.getCenter();
+            marker.setPosition(mapCenter);
+            _this.set('coords', Ember.Object.create({lat: mapCenter.k, lon: mapCenter.A}));
         });
     }
 });
